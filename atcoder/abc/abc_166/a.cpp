d9d85f82b602dcffabda0b8b30d130255f60d51c
hsmtknj <emoke.any@gmail.com>
2020-07-05 14:35:01 +0900

    solved abc_166 a, b, c

---
+++ atcoder/abc/abc_166/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int main() {
+    std::string S;
+    std::cin >> S;
+
+    if (S == "ABC")
+        std::cout << "ARC" << std::endl;
+    else if (S == "ARC")
+        std::cout << "ABC" << std::endl;
+
+    return 0;
+}
